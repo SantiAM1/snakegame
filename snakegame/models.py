b898ad48196a619450e9c1deec78a38ecc24483c
SantiAM1 <150865458+SantiAM1@users.noreply.github.com>
2024-01-08 14:04:40 -0300

    Update models.py

--- snakegame/models.py
+++ snakegame/models.py
@@ -2,7 +2,7 @@
 altura = 8
 base = 8
 
-from funciones import pos_init, snake_long
+from funciones import pos_init, snake_long, graphics
     
 class Player:
     def __init__(self, pos, body, life, manzana):
@@ -10,38 +10,50 @@ class Player:
         self.body = body
         self.life = life
         self.manzana = manzana
+        self.sentido = "up"
     
-    def up(self, display):
+    def up(self):
+        self.sentido = "up"
         if self.pos[1] == 1:
-            self.pos[1] = display.altura
+            #Fuera de rango, juego terminado
+            self.life = -1
         else:
             self.pos[1] -= 1
     
     def down(self, display):
+        self.sentido = "down"
         if self.pos[1] == display.altura:
-            self.pos[1] = 1
+            #Fuera de rango, juego terminado
+            self.life = -1
         else:
             self.pos[1] += 1
 
     def right(self, display):
+        self.sentido = "right"
         if self.pos[0] == display.base:
-            self.pos[0] = 1
+            #Fuera de rango, juego terminado
+            self.life = -1
         else:
             self.pos[0] += 1
     
-    def left(self, display):
+    def left(self):
+        self.sentido = "left"
         if self.pos[0] == 1:
-            self.pos[0] = display.base
+            #Fuera de rango, juego terminado
+            self.life = -1
         else:
             self.pos[0] -= 1
 
     def colision_test(self):
+        #Detectar si el jugador choco consigo mismo
         for element_body in self.body:
             if self.pos == element_body:
                 self.life = -1
                 break
+        #Detectar que el juegador come una manzana
         if self.pos == self.manzana:
             self.life += 1
+            #Nueva pos de manzana
             while True:
                 self.manzana = pos_init(altura, base)
                 manzana_valida = True
@@ -56,6 +68,7 @@ class Player:
                     break
 
     def leng_body(self, ultimo):
+        #Generar el largo de la serpiente
         self.body = snake_long(self.body, ultimo.copy(), self.life)
 
     def __str__(self):
@@ -79,23 +92,96 @@ class Display:
         manzana_y = player.manzana[1]
 
         filas = []
+        
+        #Importar vectores graficos
+        graph = graphics(player)
 
-        for i in range(1, self.altura+1):
-            fila = ["-"]*self.base
+        for i in range(1, self.altura + 1):
+            fila = [" "]*self.base
 
             if i == manzana_y:
                 fila[manzana_x] = "O"
+            
+            #Generar body de la serpiente con graphics
+            for num in range(len(player.body)):
+                if player.body[num][1] == i:
 
-            for element_body in player.body:
-                if element_body[1] == i:
-                    fila[element_body[0]-1] = "x"
+                    if num < len(graph) and graph[num] == [1, 0] and graph[num-1] == [1, 0]:
+                        fila[player.body[num][0]-1] = "="
+                    if num < len(graph) and graph[num] == [-1, 0] and graph[num-1] == [-1, 0]:
+                        fila[player.body[num][0]-1] = "="
 
+                    if num < len(graph) and graph[num] == [0, 1] and graph[num-1] == [0, 1]:
+                        fila[player.body[num][0]-1] = "║"
+                    if num < len(graph) and graph[num] == [0, -1] and graph[num-1] == [0, -1]:
+                        fila[player.body[num][0]-1] = "║"
+                    
+                    if num < len(graph) and graph[num] == [0, 1] and graph[num-1] == [1, 0]:
+                        fila[player.body[num][0]-1] = "╚"
+                    if num < len(graph) and graph[num] == [0, 1] and graph[num-1] == [-1, 0]:
+                        fila[player.body[num][0]-1] = "╝"
+                    
+                    if num < len(graph) and graph[num] == [0, -1] and graph[num-1] == [1, 0]:
+                        fila[player.body[num][0]-1] = "╔"
+                    if num < len(graph) and graph[num] == [0, -1] and graph[num-1] == [-1, 0]:
+                        fila[player.body[num][0]-1] = "╗"
+                    
+                    if num < len(graph) and graph[num] == [1, 0] and graph[num-1] == [0, 1]:
+                        fila[player.body[num][0]-1] = "╗"
+                    if num < len(graph) and graph[num] == [1, 0] and graph[num-1] == [0, -1]:
+                        fila[player.body[num][0]-1] = "╝"
+
+                    if num < len(graph) and graph[num] == [-1, 0] and graph[num-1] == [0, 1]:
+                        fila[player.body[num][0]-1] = "╔"
+                    if num < len(graph) and graph[num] == [-1, 0] and graph[num-1] == [0, -1]:
+                        fila[player.body[num][0]-1] = "╚"
+
+                    if num == 0 and num < len(graph) and player.sentido == "up" and graph[num] == [1, 0]:
+                        fila[player.body[num][0]-1] = "╝"
+                    if num == 0 and num < len(graph) and player.sentido == "up" and graph[num] == [-1, 0]:
+                        fila[player.body[num][0]-1] = "╚"
+                    if num == 0 and num < len(graph) and player.sentido == "up" and (graph[num] == [0, 1] or graph[num] == [0, -1]):
+                        fila[player.body[num][0]-1] = "║"
+
+                    if num == 0 and num < len(graph) and player.sentido == "down" and graph[num] == [-1, 0]:
+                        fila[player.body[num][0]-1] = "╔"
+                    if num == 0 and num < len(graph) and player.sentido == "down" and graph[num] == [1, 0]:
+                        fila[player.body[num][0]-1] = "╗"
+                    if num == 0 and num < len(graph) and player.sentido == "down" and (graph[num] == [0, 1] or graph[num] == [0, -1]):
+                        fila[player.body[num][0]-1] = "║"
+
+                    if num == 0 and num < len(graph) and player.sentido == "left" and graph[num] == [0, 1]:
+                        fila[player.body[num][0]-1] = "╝"
+                    if num == 0 and num < len(graph) and player.sentido == "left" and graph[num] == [0, -1]:
+                        fila[player.body[num][0]-1] = "╗"
+                    if num == 0 and num < len(graph) and player.sentido == "left" and (graph[num] == [1, 0] or graph[num] == [-1, 0]):
+                        fila[player.body[num][0]-1] = "="   
+
+                    if num == 0 and num < len(graph) and player.sentido == "right" and graph[num] == [0, 1]:
+                        fila[player.body[num][0]-1] = "╚"
+                    if num == 0 and num < len(graph) and player.sentido == "right" and graph[num] == [0, -1]:
+                        fila[player.body[num][0]-1] = "╔"
+                    if num == 0 and num < len(graph) and player.sentido == "right" and (graph[num] == [1, 0] or graph[num] == [-1, 0]):
+                        fila[player.body[num][0]-1] = "="   
+                    
+                    if num == len(player.body)-1:
+                        fila[player.body[num][0]-1] = "x"
+
+            #Cabeza de la serpiente
             if i == posy:
-                fila[posx] = "@"
+                if player.sentido == "up":
+                    fila[posx] = "^"
+                if player.sentido == "down":
+                    fila[posx] = "v"
+                if player.sentido == "right":
+                    fila[posx] = ">"
+                if player.sentido == "left":
+                    fila[posx] = "<"
 
             fila = " ".join(fila)
             filas.append(fila)
 
-        filas.insert(0,"█"+"██" * (self.base-1))
-        filas.append("█"+"██" * (self.base-1))
+        #Bordes
+        filas.insert(0, "█" + "██" * (self.base-1))
+        filas.append("█" + "██" * (self.base-1))
         return "\n".join(filas)
